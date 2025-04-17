942bc13017fcedfc5c480248bdf3357525ccf7e0
DoyleMartin <Doylecmartin@gmail.com>
2025-04-17 13:04:41 -0400

    AuthAPI changes

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -2,6 +2,28 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}` );
+    }
+    const data = await response.json();
+
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+    }
+    return data;
+    } catch (error) {
+      console.error('Error during login:', error);
+      throw error;  
+  }
 }
 
 
--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,10 +3,13 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
@@ -15,16 +18,25 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('token');
+    if (loggedUser) {
+      return loggedUser;
+    }
+    return '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('token');
+    window.location.assign('/login');
   }
 }
 
